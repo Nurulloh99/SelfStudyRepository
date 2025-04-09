1ea9d8235c5a460d1d3f3cd919c5c04343fe0b16
Nurulloh99 <nurullohlutfullayev1999@gmail.com>
2025-04-09 18:46:47 +0500

    Add project files.

---
+++ Bekmurod/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Bekmurod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
